e5e39cfc9ee33292a004780dfb5686d6f6211846
familia-af <familia.ag.fi@gmail.com>
2025-03-16 19:17:43 -0600

    Agregue el segundo ejercicio.

---
+++ cuarta-seccion/ejercicio2/app.js
@@ -0,0 +1,13 @@
+// Ejercicio 2: Uso de estructuras condicionales
+function evaluarNumero(n) {
+  if (n > 0) {
+    console.log("El número es positivo");
+  } else if (n < 0) {
+    console.log("El número es negativo");
+  } else {
+    console.log("El número es cero");
+  }
+}
+evaluarNumero(10);
+evaluarNumero(-5);
+evaluarNumero(0);
