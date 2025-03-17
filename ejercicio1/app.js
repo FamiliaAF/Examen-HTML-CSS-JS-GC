49ad1f4b480088052bd5217fca0b1d5f7e666edd
familia-af <familia.ag.fi@gmail.com>
2025-03-16 19:14:17 -0600

    Cree la cuarta-seccion y el primer ejercicio.

---
+++ ejercicio1/app.js
@@ -0,0 +1,9 @@
+// Ejercicio 1: Manipulación de variables y operadores
+let a = 10, b = 5, c = 3;
+let resultado1 = (a + b) * c;
+let residuo = a % b;
+let cFlotante = parseFloat(c.toFixed(2));
+
+console.log("Resultado de (a + b) * c:", resultado1);
+console.log("Residuo de a / b:", residuo);
+console.log("Valor de c con dos decimales:", cFlotante);
